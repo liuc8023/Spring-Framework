d678ec8610f374df516bf799afbafca2b82318a7
liuc8023 <13764402356@163.com>
2021-02-17 02:21:19 +0800

    Update MainStarter.java

--- spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainStarter.java
+++ spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainStarter.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MainStarter {
 	public static void main(String[] args) {
+		/**
+		 * 使用AnnotationConfigApplicationContext可以实现基于Java的配置类加载Spring的应用上下文。
+		 * 避免使用application.xml进行配置。相比XML配置，更加便捷。
+		 */
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
 		Person person = (Person) context.getBean("person");
 		System.out.println("person:"+person);
