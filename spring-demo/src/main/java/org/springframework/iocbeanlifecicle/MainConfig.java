843dbeddb48696e87d12aa29e3173672ccb71767
liuc8023 <13764402356@163.com>
2021-02-17 02:21:26 +0800

    Update MainConfig.java

--- spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainConfig.java
+++ spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainConfig.java
@@ -4,11 +4,14 @@ package org.springframework.iocbeanlifecicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+//@Configuration可理解为用spring的时候xml里面的<beans>标签
 @Configuration
 @ComponentScan(basePackages = "org.springframework.iocbeanlifecicle")
 public class MainConfig {
 
+	/**
+	 * @Bean可理解为用spring的时候xml里面的<bean>标签
+	 */
 	@Bean
 	public Person person(){
 		Person person = new Person();
