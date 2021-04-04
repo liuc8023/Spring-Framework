63797bd759db8346dd0022b658cdc9d454a2d48b
liuc8023 <13764402356@163.com>
2021-04-04 22:56:50 +0800

    Update MainStarter.java

--- spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainStarter.java
+++ spring-demo/src/main/java/org/springframework/iocbeanlifecicle/MainStarter.java
@@ -10,6 +10,12 @@ public class MainStarter {
 		 * 避免使用application.xml进行配置。相比XML配置，更加便捷。
 		 */
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		for(String name : beanDefinitionNames){
+			System.out.println(name);
+		}
+		// 如果基于XML文件配置，则也可以如下：
+		// ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
 		Person person = (Person) context.getBean("person");
 		System.out.println("person:"+person);
 	}
